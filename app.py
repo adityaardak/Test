2ffd492fc91e2cfdc8375b23f8e993d730e2d568
Aditya Ardak <58509583+adityaardak@users.noreply.github.com>
2025-01-30 10:41:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello, Streamlit!")
-st.write("This is your first Streamlit app.")
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
